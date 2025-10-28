44f2e12953ddab43d188e73b1b31a4edb72a253f
Fernando Bryan Reza Campos <fer.rezac@outlook.com>
2025-10-28 09:25:02 -0600

    Add export fn's & main tester.py script on examples

---
+++ examples/main_tester.py
@@ -0,0 +1,10 @@
+from gwa import __version__
+
+
+def main():
+    print("This is the main tester for the gwa package.")
+    print(f"gwa version: {__version__}")
+
+if __name__ == "__main__":
+    main()
+
--- src/gwa/__init__.py
+++ src/gwa/__init__.py
@@ -1,5 +1,18 @@
 from gwa._core import hello_from_bin
 
 
+
+
 def main() -> None:
     print(hello_from_bin())
+
+# * Export fn's
+def export_version() -> str:
+    from importlib.metadata import version
+    global __version__
+    __version__ = version("gwa")
+    return __version__
+
+# * Export callouts
+export_version()
+# export_some_other_callout()
